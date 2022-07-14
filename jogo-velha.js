ff18100e8ac20919d92e3a79f0813a1e239252e2
Mrcellyss <109051462+Mrcellyss@users.noreply.github.com>
2022-07-14 19:39:01 -0300

    Lógica do jogo da velha

--- jogo-velha.js
+++ jogo-velha.js
@@ -1,10 +1,97 @@
 const X = "X";
 const O = "O";
 
-function reiniciarJogo() {
-    
+let i ;
+let jogador = "X";
+
+marcarJogadorAtivo(jogador);
+
+function proximoJogador ( );{
+    jogador = jogador === "X" ? "O" : "X" ;
+    marcarJogadorAtivo(jogador);
 }
+
+// X vencedor na horizontal
+for (i = 1; i &lt;= 3 ; i ++ ){
+    if ( desenharSimbolo(X, i, 1) = selecionarArea (i,1) &amp;&amp;
+        desenharSimbolo(X, i, 2) = selecionarArea (i,2) &amp;&amp;
+        desenharSimbolo(X, i, 3) = selecionarArea ( i,3) {
+        exibirResultado(Vencedor);
+    }
+    Else {exibirResultado(Empate);
+    }
+}
+
+// x vencedor na vertical
+for ( i = 1; i &lt;= 3 ; i ++){
+    if ( desenharSimbolo(X, 1, i) = selecionarArea (1,i) &amp;&amp;
+        desenharSimbolo(X, 2, i) = selecionarArea (2,i) &amp;&amp;
+        desenharSimbolo(X, 3, i) = selecionarArea ( 3,i) {
+        exibirResultado(Vencedor);
+    }
+    Else {exibirResultado(Empate);
+    }
+}
+
+// x vencedor na diagonal
+    if ( desenharSimbolo(X, 1, 1) = selecionarArea (1,1) &amp;&amp;
+        desenharSimbolo(X,2, 2) = selecionarArea (2,2) &amp;&amp;
+        desenharSimbolo(X, 3, 3) = selecionarArea ( 3,3) {
+        exibirResultado(Vencedor);
+    }
+    Else {exibirResultado(Empate);
+}
+
+    if ( desenharSimbolo(X,1, 3) = selecionarArea (1,3) &amp;&amp;
+        desenharSimbolo(X,2, 2) = selecionarArea (2,2) &amp;&amp;
+        desenharSimbolo(X,3, 1) = selecionarArea (3,1) {
+        exibirResultado(Vencedor);
+    }
+    Else {exibirResultado(Empate);
+    }
+
+// O vencedor na horizontal
+for (i = 1; i &lt;= 3 ; i ++ ){
+    if ( desenharSimbolo(O, i, 1) = selecionarArea (i,1) &amp;&amp;
+        desenharSimbolo(O, i, 2) = selecionarArea (i,2) &amp;&amp;
+        desenharSimbolo(O, i, 3) = selecionarArea ( i,3) {
+        exibirResultado(Vencedor);
+    }
+    Else {exibirResultado(Empate);
+    }
+}
+
+// x vencedor na vertical
+for ( i = 1; i &lt;= 3 ; i ++){
+    if ( desenharSimbolo(O, 1, i) = selecionarArea (1,i) &amp;&amp;
+        desenharSimbolo(O, 2, i) = selecionarArea (2,i) &amp;&amp;
+        desenharSimbolo(O, 3, i) = selecionarArea ( 3,i) {
+        exibirResultado(Vencedor);
+    }
+    Else {exibirResultado(Empate);
+    }
+}
+// x vencedor na diagonal
+    if ( desenharSimbolo(O, 1, 1) = selecionarArea (1,1) &amp;&amp;
+        desenharSimbolo(O,2, 2) = selecionarArea (2,2) &amp;&amp;
+        desenharSimbolo(O, 3, 3) = selecionarArea ( 3,3) {
+        exibirResultado(Vencedor);
+    }
+    Else {exibirResultado(Empate);
+    }
+
+    if ( desenharSimbolo(O,1, 3) = selecionarArea (1,3) &amp;&amp;
+        desenharSimbolo(O,2, 2) = selecionarArea (2,2) &amp;&amp;
+        desenharSimbolo(O,3, 1) = selecionarArea (3,1) {
+        exibirResultado(Vencedor);
+    }
+        Else {exibirResultado(Empate);
+    }
 
 function selecionarArea(posicaoLinha, posicaoColuna) {
 
+}
+
+function reiniciarJogo() {
+    location.reload();
 }
